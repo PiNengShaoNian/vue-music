725ffac27e6edb097408f8254bdc50d2c27de769
Lao Qu <quguiyou1076@qq.com>
2019-08-13 14:44:27 +0800

    init project directory and finished header component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <m-header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from './components/m-header/m-header'
+
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+</style>
